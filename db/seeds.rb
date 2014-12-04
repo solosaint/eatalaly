208fbc8c8b10b511a3281a72ddf997b8871e8da7
rob wilke <rob@justdothis.com>
2014-12-04 14:26:19 -0500

    added db seeds and food types and prefs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+food_types = ["Breakfasts", "Dessert", "Sides","Lunches", "Dinners", "Pre-workout", "Post-workout", "mid-Day snacks", "mid-Morning snacks"]
+food_types.each{|d| FoodType.where(:name => d).first_or_create}
+food_preferences = ["Vegetarian", "Vegan","Meat","Dairy", "Low-Carb", "Paleo", "Macrobiotic", "Whole-30"]
+food_preferences.each{|d| FoodPreference.where(:name => d).first_or_create}
+cuisines = ["American", "Italian", "Thai", "Asian", "Muscle Man", "Mexican", "Indian","Chinese"]
+cuisines.each{|d| Cuisine.where(:name => d).first_or_create}
