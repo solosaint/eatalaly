fb6982b0014f2293ab85ea5a4d5a52bc773757e1
rob wilke <rob@justdothis.com>
2014-12-04 17:18:17 -0500

    added difficulty level

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ActiveRecord::Base
+
+  #associations
+  belongs_to :food_preference
+  belongs_to :food_type
+  belongs_to :cuisine
+
+DIFFICULTY = [
+              "Easy" ,
+              "Medium" ,
+              "Hard"
+  ]
 end
