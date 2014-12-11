44029bacbb361e0de614b2aa0d4e8e02ee4825fb
rob wilke <rob@justdothis.com>
2014-12-11 16:11:37 -0500

    pushing changes for this

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ActiveRecord::Base
 
-  #associations
   belongs_to :food_preference
   belongs_to :food_type
   belongs_to :cuisine
--- db/migrate/20141204191137_create_food_preferences.rb
+++ db/migrate/20141204191137_create_food_preferences.rb
@@ -1,6 +1,7 @@
 class CreateFoodPreferences < ActiveRecord::Migration
   def change
     create_table :food_preferences do |t|
+    	t.string :name
 
       t.timestamps
     end
